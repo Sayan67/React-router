8c4cb75f32771a3c14d909e66d92c5756ce027dc
Sayan67 <sayan.das.21@aot.edu.in>
2023-12-04 16:07:59 +0530

    Improved UI impelmented glass morph effect

--- to-do-list/src/app/page.tsx
+++ to-do-list/src/app/page.tsx
@@ -17,15 +17,16 @@ export default function Home() {
     setDesc("");
 
   }
-  let taskRender : JSX.Element[] =[( <div className='px-5 py-2 rounded-md w-full mb-5 h-fit bg-white backdrop-blur-sm text-center'>No task Pending</div>)]
+  let taskRender : JSX.Element[] =[( <div className='w-[80%] px-5 py-2 rounded-md mb-5 h-fit bg-white bg-opacity-30 backdrop-blur-sm drop-shadow-xl text-center'>No task Pending</div>)]
   if(mainTask.length>0){
     taskRender  = mainTask.map((item, i) => {
         return (<>
-          <div className='px-5 py-2 rounded-md mb-5 w-[80%] h-fit bg-white backdrop-blur-sm text-center'>
-            <h2 className='text-xl font-semibold'>
+          <div className='Hello grid grid-cols-3 px-5 py-2 rounded-md mb-5 w-[80%] h-fit bg-opacity-30 bg-white backdrop-blur-sm text-center drop-shadow-xl'>
+            <h2 className=' text-xl font-semibold w-full col-span-2 px-5 py-2 '>
               {item.title} 
             </h2>
-            <h3>
+            <button className=' w-fit flex h-fit px-5 py-2 rounded-md bg-red-500 text-white bg-opacity-80'>Delete</button>
+            <h3 className='col-span-2 w-full px-5 py-2 '>
               {item.desc}
             </h3>
           </div>
@@ -39,30 +40,30 @@ export default function Home() {
 
   return (
     <>
-      <div className=' w-full h-[100%] font-sans bg-purple-100'>
-        <h1 className='bg-purple-500 p-6 text-center text-white font-bold text-3xl'>{name}'s To do list</h1>
+      <div className='flex-grow w-full h-screen font-sans bg-cover bg-[url(https://www.slingacademy.com/wp-content/uploads/2022/10/green.webp)]'>
+        <h1 className='bg-white bg-opacity-25 backdrop-blur-md drop-shadow-xl p-6 text-center text-white font-bold text-3xl'>{name}'s To do list</h1>
         <div className='flex py-14 h-fit justify-center mx-7'>
           <div className='flex gap-2 w-fit'>
-            <form className='' action="" onSubmit={() => { submitHandler }}>
-              <input className='px-2 mb-3 py-1 h-fit rounded-md border-2 border-purple-600 w-[100%]' type="text" placeholder='Enter your task'
+            <form className=' px-10 py-16 bg-white bg-opacity-30 shadow-xl drop-shadow-xl rounded-md backdrop-blur-md' action="" onSubmit={() => { submitHandler }}>
+              <input className='px-2 mb-3 py-1 h-fit placeholder-white rounded-md border-2 bg-white text-white bg-opacity-25 border-gray-400 w-[100%]' type="text" placeholder='Enter your task'
                 value={title}
                 onChange={(e) => {
                   setTitle(e.target.value);
                 }} />
-              <textarea placeholder='Enter your description' className='px-2 py-1 mb-3 rounded-md border-2 border-purple-600 w-[100%] h-20' name="" id="" cols={30} rows={10}
+              <textarea placeholder='Enter your description' className='px-2 py-1 mb-3 placeholder-white bg-white bg-opacity-25 text-white rounded-md border-2 border-gray-400 w-[100%] h-20' name="" id="" cols={30} rows={10}
                 value={desc}
                 onChange={(e) => {
                   setDesc(e.target.value);
                 }}
               ></textarea>
               <div className='flex w-full h-fit'>
-                <button className='h-10 w-full m-auto rounded-md py-1.5 px-6 bg-purple-600 text-white' onClick={submitHandler}>Save</button>
+                <button className='h-10 mt-5 w-full m-auto rounded-md drop-shadow-2xl py-1.5 px-6 shadow-xl bg-black bg-opacity-50 text-white ' onClick={submitHandler}>Save</button>
               </div>
             </form>
           </div>
 
         </div>
-        <div className='grid h-fit place-items-center'>
+        <div className='grid h-fit place-items-center bg-transparent '>
           {taskRender}
         </div>
       </div>
